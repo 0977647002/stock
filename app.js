09f90babfe945bad7c860e902ae35d6040d68ef5
Julien Bisconti <julienbi@unity3d.com>
2021-07-16 08:52:47 +0700

    chane UserStockApi.js

--- app.js
+++ app.js
@@ -1,33 +1,22 @@
 const express = require("express");
-const session = require("express-session");
-const cookie = require("cookie-parser");
 const categoryRouter = require("./app/api/CategoryApi");
 const stockRouter = require("./app/api/StockApi");
+const userStockRouter = require('./app/api/UserStockApi');
 const loginRouter = require("./app/api/loginAPI");
-const userStockRouter = require("./app/api/UserStockApi");
 const athen = require("./app/jwt/authentycation");
-const app = express();
 const cors = require("cors");
 
-app.use(
-  session({
-    secret: "this-is-a-secret-token",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 100 * 60 * 60 * 60 * 24 },
-  })
-);
+const app = express();
 
 app.use(cors());
 
-app.use(cookie());
 app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/api/category", categoryRouter);
 app.use("/api/stock", stockRouter);
 app.use("/api", loginRouter);
-app.use("/api/user/stock", athen.verifyToken, userStockRouter);
+app.use("/api/user/stock", userStockRouter);
 
 const PORT = process.env.SERVER_PORT || 8080;
 
